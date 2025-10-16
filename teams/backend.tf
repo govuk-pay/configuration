f81ac7507d7502d4ce5e92350b50ecc999206628
Joe Roberts <joe.roberts@digital.cabinet-office.gov.uk>
2025-10-16 14:34:12 +0100

    Add backend configuration

--- teams/backend.tf
+++ teams/backend.tf
@@ -1,9 +1,21 @@
-/*terraform {
+terraform {
+  required_version = ">=1.9.6, <2.0.0"
+
   backend "s3" {
-    bucket         = "govuk-pay-github-tfstate"
-    key            = "govuk-pay-admin-terraform.tfstate"
-    region         = "eu-west-2"
-    dynamodb_table = "govuk-pay-github-terraform-state-lock"
-    encrypt        = true
+    bucket         = "pay-govuk-terraform-state-deploy"
+    key            = "github-configuration.tfstate"
+    region         = "eu-west-1"
+    acl            = "bucket-owner-read"
+    dynamodb_table = "terraform_locks"
   }
-}*/
+
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~>6.6.0"
+    }
+  }
+}
+
+provider "github" {
+}
